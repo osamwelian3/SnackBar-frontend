1fd9ccb596ed66da6f8358b2387161d4e4175641
Samuel Ian <osamwelian3@gmail.com>
2022-08-18 23:03:27 +0300

    It will add handler for input change and submit events

--- src/App.js
+++ src/App.js
@@ -2,13 +2,34 @@ import React, { Component, Fragment } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Navbar from './components/navbar';
+import Register from './components/auth/register';
 
 class App extends Component {
-  state = {  } 
+  state = { 
+    username: 'test',
+    email: '',
+    password: '',
+    password2: ''
+  } 
+
+  handleSubmit = (event) => {
+    event.preventDefault();
+    console.log(this.state)
+  }
+
+  handleInputChange = (e) => {
+    const { name, value } = e.target
+    console.log({[name]: value})
+    this.setState({
+      [name]: value
+    });
+  }
+
   render() { 
     return (
       <Fragment >
         <Navbar />
+        <Register credentials={this.state} onSubmit={this.handleSubmit} onChange={this.handleInputChange} />
       </Fragment>
     );
   }
